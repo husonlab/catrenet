19afa4caa320136def77f9d154865d9441bfef48
Daniel <daniel.huson@uni-tuebingen.de>
2022-03-11 07:51:15 +0100

    minor fixes

--- src/catlynet/main/CatlyNet.java
+++ src/catlynet/main/CatlyNet.java
@@ -108,6 +108,7 @@ public class CatlyNet extends Application {
         final boolean showVersion = options.getOption("-V", "version", "Show version string", false);
         final boolean silentMode = options.getOption("-S", "silentMode", "Silent mode", false);
         ProgramExecutorService.setNumberOfCoresToUse(options.getOption("-t", "threads", "Maximum number of threads to use in a parallel algorithm (0=all available)", 0));
+        ProgramProperties.setConfirmQuit(options.getOption("-q", "confirmQuit", "Confirm quit on exit", ProgramProperties.isConfirmQuit()));
         options.done();
 
         ProgramProperties.load(propertiesFile);
